75e9809a5374ca6c6571875c69185580f9670f2c
Emporer4Life <aiden9874@gmail.com>
2019-02-24 12:46:43 +1300

    Added initialise_main_screen and main_screen_format functions to make main file easier to read.

--- calculator.py
+++ calculator.py
@@ -13,18 +13,26 @@ def new_screen(fnc):
     fnc()
 
 
-# Initialise main
+def initialise_main_screen(screen):
+    screen.title("Aiden's Calculator")
+    screen.geometry('400x400')
+    screen.configure(background=settings.BLACK)
+
+
+def main_screen_format(screen):
+    Label(screen, text='Aiden\'s Calculator\nMain Screen', fg=settings.WHITE, bg=settings.BLACK, font=settings.title_font) \
+        .grid(row=0, column=0, sticky=NSEW)
+    Button(screen, text='Calculate ending capital', width=18, command=lambda: new_screen(compound_calculator.start_calc)) \
+        .grid(row=1, column=0, sticky=NSEW)
+    Button(screen, text='Year by year', width=18, command=lambda: new_screen(year_by.start_calc)) \
+        .grid(row=2, column=0, sticky=NSEW)
+
+
+# Open main window
 main_screen = Tk()
-main_screen.title("Aiden's Calculator")
-main_screen.geometry('400x400')
-main_screen.configure(background=settings.BLACK)
-#
-Label(main_screen, text='Aiden\'s Calculator\nMain Screen', fg=settings.WHITE, bg=settings.BLACK, font=settings.title_font) \
-    .grid(row=0, column=0, sticky=NSEW)
-Button(main_screen, text='Calculate ending capital', width=18, command=lambda: new_screen(compound_calculator.start_calc)) \
-    .grid(row=1, column=0, sticky=NSEW)
-Button(main_screen, text='Year by year', width=18, command=lambda: new_screen(year_by.start_calc)) \
-    .grid(row=2, column=0, sticky=NSEW)
+initialise_main_screen(main_screen)
+# Add widgets to main window
+main_screen_format(main_screen)
 
 print('hi')
 # Center everything

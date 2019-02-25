243527dff7e9eedaeb658e4ac95a70cbe8c3d3e4
Emporer4Life <aiden9874@gmail.com>
2019-02-25 16:20:45 +1300

    Made class LineGraph and added all existing functions under it.

--- multi_line_annot.py
+++ multi_line_annot.py
@@ -1,54 +1,53 @@
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
+from tkinter import *
+
+class LineGraph:
+
+    def __init__(self):
+        self.fig, self.ax = plt.subplots()
+
+    def update_annot(self, ind, line, annot, ydata):
+        x, y = line.get_data()
+        annot.xy = (x[ind["ind"][0]], y[ind["ind"][0]])
+        # Get x and y values, then format them to be displayed
+        x_values = " ".join(list(map(str, ind["ind"])))
+        y_values = " ".join(str(ydata[n]) for n in ind["ind"])
+        # Format of label can be changed here
+        text = "{}, {}".format(x_values, y_values)
+        annot.set_text(text)
+        annot.get_bbox_patch().set_alpha(0.4)
 
 
-def update_annot(ind, line, annot, ydata):
-    x, y = line.get_data()
-    annot.xy = (x[ind["ind"][0]], y[ind["ind"][0]])
-    # Get x and y values, then format them to be displayed
-    x_values = " ".join(list(map(str, ind["ind"])))
-    y_values = " ".join(str(ydata[n]) for n in ind["ind"])
-    # Format of label can be changed here
-    text = "{}, {}".format(x_values, y_values)
-    annot.set_text(text)
-    annot.get_bbox_patch().set_alpha(0.4)
+    def hover(self, event, line, annot, ydata):
+        vis = annot.get_visible()
+        if event.inaxes == self.ax:
+            # Draw annotations if cursor in right position
+            cont, ind = line.contains(event)
+            if cont:
+                self.update_annot(ind, line, annot, ydata)
+                annot.set_visible(True)
+                self.fig.canvas.draw_idle()
+            else:
+                # Don't draw annotations
+                if vis:
+                    annot.set_visible(False)
+                    self.fig.canvas.draw_idle()
 
 
-def hover(event, line, annot, ydata):
-    vis = annot.get_visible()
-    if event.inaxes == ax:
-        # Draw annotations if cursor in right position
-        cont, ind = line.contains(event)
-        if cont:
-            update_annot(ind, line, annot, ydata)
-            annot.set_visible(True)
-            fig.canvas.draw_idle()
-        else:
-            # Don't draw annotations
-            if vis:
-                annot.set_visible(False)
-                fig.canvas.draw_idle()
+    def plot_line(self, x, y):
+        line, = plt.plot(x, y, marker="o")
+        # Annotation style may be changed here
+        annot = self.ax.annotate("", xy=(0, 0), xytext=(-20, 20), textcoords="offset points",
+                            bbox=dict(boxstyle="round", fc="w"),
+                            arrowprops=dict(arrowstyle="->"))
+        annot.set_visible(False)
+        self.fig.canvas.mpl_connect("motion_notify_event",
+                               lambda event: self.hover(event, line, annot, y))
 
-
-def plot_line(x, y):
-    line, = plt.plot(x, y, marker="o")
-    # Annotation style may be changed here
-    annot = ax.annotate("", xy=(0, 0), xytext=(-20, 20), textcoords="offset points",
-                        bbox=dict(boxstyle="round", fc="w"),
-                        arrowprops=dict(arrowstyle="->"))
-    annot.set_visible(False)
-    fig.canvas.mpl_connect("motion_notify_event",
-                           lambda event: hover(event, line, annot, y))
-
-
-def init_graph():
-    fig, ax = plt.subplots()
-
-
-def show_graph():
-    plt.show()
-
-
-fig, ax = plt.subplots()
+    def show_graph(self):
+        plt.show()
 
 
 if __name__ == '__main__':
@@ -58,7 +57,7 @@ if __name__ == '__main__':
     x2 = range(21)
     y2 = range(0, 42, 2)
     # Plot line graphs
-    fig, ax = plt.subplots()
-    plot_line(x1, y1)
-    plot_line(x2, y2)
+    new = LineGraph()
+    new.plot_line(x1, y1)
+    new.plot_line(x2, y2)
     plt.show()

ec4f14ea7cd4c2b2bad2da2f2b60d58d2b3de856
Emporer4Life <aiden9874@gmail.com>
2019-02-26 14:28:51 +1300

    Updated calculator.py to class structure

--- calculator.py
+++ calculator.py
@@ -2,41 +2,45 @@
 Visual calculator in python for interest rates
 Author: Aiden Burgess
 '''
-from tkinter import *
+import tkinter as tk
 import settings
 import compound_calculator
 import year_by
 
 
-def new_screen(fnc):
-    main_screen.destroy()
-    fnc()
+class MainScreen:
+
+    def __init__(self):
+        # Open main window
+        self.screen = tk.Tk()
+        self.init_main_screen()
+        # Add widgets to main window
+        self.format_main_screen()
+        # Run the window
+        self.screen.mainloop()
+
+    def new_screen(self, fnc):
+        self.screen.destroy()
+        fnc()
+
+    def init_main_screen(self):
+        self.screen.title("Aiden's Calculator")
+        self.screen.geometry('400x400')
+        self.screen.configure(background=settings.BLACK)
+
+    def format_main_screen(self):
+        tk.Label(self.screen, text='Aiden\'s Calculator\nMain Screen', fg=settings.WHITE, bg=settings.BLACK, font=settings.title_font) \
+            .grid(row=0, column=0, sticky=tk.NSEW)
+        tk.Button(self.screen, text='Calculate ending capital', width=18, command=lambda: self.new_screen(compound_calculator.start_calc)) \
+            .grid(row=1, column=0, sticky=tk.NSEW)
+        tk.Button(self.screen, text='Year by year', width=18, command=lambda: self.new_screen(year_by.start_calc)) \
+            .grid(row=2, column=0, sticky=tk.NSEW)
+        # Center everything
+        self.screen.grid_rowconfigure(0, weight=2)
+        self.screen.grid_rowconfigure(1, weight=1)
+        self.screen.grid_rowconfigure(2, weight=1)
+        self.screen.grid_columnconfigure(0, weight=1)
 
 
-def initialise_main_screen(screen):
-    screen.title("Aiden's Calculator")
-    screen.geometry('400x400')
-    screen.configure(background=settings.BLACK)
-
-
-def main_screen_format(screen):
-    Label(screen, text='Aiden\'s Calculator\nMain Screen', fg=settings.WHITE, bg=settings.BLACK, font=settings.title_font) \
-        .grid(row=0, column=0, sticky=NSEW)
-    Button(screen, text='Calculate ending capital', width=18, command=lambda: new_screen(compound_calculator.start_calc)) \
-        .grid(row=1, column=0, sticky=NSEW)
-    Button(screen, text='Year by year', width=18, command=lambda: new_screen(year_by.start_calc)) \
-        .grid(row=2, column=0, sticky=NSEW)
-    # Center everything
-    screen.grid_rowconfigure(0, weight=2)
-    screen.grid_rowconfigure(1, weight=1)
-    screen.grid_rowconfigure(2, weight=1)
-    screen.grid_columnconfigure(0, weight=1)
-
-
-# Open main window
-main_screen = Tk()
-initialise_main_screen(main_screen)
-# Add widgets to main window
-main_screen_format(main_screen)
-# Run the window
-main_screen.mainloop()
+if __name__ == '__main__':
+    MainScreen()

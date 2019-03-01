ca287b7642378c62ad01beb406c5bd2337f538d7
Emporer4Life <aiden9874@gmail.com>
2019-03-01 17:29:30 +1300

    Changed year_by to same script as compound_calculator.

--- year_by.py
+++ year_by.py
@@ -4,109 +4,156 @@ interest rate, starting capital, number of years, and yearly contribution.
 
 Author: Aiden Burgess
 '''
-from tkinter import *
+import tkinter as tk
+import multi_line_annot as mla
 import settings
-
-
-def start_calc():
-    YearBy()
-
+import calculator
 
 class YearBy:
 
     def __init__(self):
         # Open new window for
-        self.screen = Tk()
+        self.screen = tk.Tk()
         self.screen.title('Compound Interest Calculator')
         self.screen.configure(background=settings.BLACK)
         # Build widgets onto window
-        self.display_title()
-        self.display_entry_boxes()
-        self.display_radio_buttons()
-        self.display_sequence_buttons()
-        self.output_ending_capital()
-        # Build graph
+        self.disp_title()
+        self.disp_entry_boxes()
+        self.disp_rad_buttons()
+        self.disp_submit_buttons()
+        self.out_ending_cap()
         # Back button
+        self.disp_back_button()
         # Quit button
         # Start the window
         print('Initialised Capital Calculator')
         self.screen.mainloop()
 
-    def display_title(self):
-        Label(self.screen, text='Year By Year Calculator', fg=settings.WHITE, bg=settings.BLACK,
-              font=settings.title_font).grid(row=0, column=0, sticky=NSEW)
+    def disp_title(self):
+        tk.Label(self.screen, text='Compound Interest', fg=settings.WHITE,
+                 bg=settings.BLACK, font=settings.title_font)\
+                 .grid(row=0, column=0, sticky=tk.NSEW)
 
     def display_text(self, text, textbox):
-        textbox.delete(0.0, END)
-        textbox.insert(END, text)
+        textbox.delete(0.0, tk.END)
+        textbox.insert(tk.END, text)
 
-    def display_entry_boxes(self):
+    def disp_entry_boxes(self):
         # Display all entry boxes and add them to a list, so they are callable
         self.entries = []
         entry_list = [('Starting Capital ($)', 1), ('Interest Rate (%)', 2),
                       ('Number of Years', 3), ('Yearly Contribution', 4)]
         for name, row in entry_list:
-            Label(self.screen, text=name + ':', fg=settings.WHITE, bg=settings.BLACK,
-                  font=settings.menu_font).grid(row=row, column=0, sticky=W)
-            self.entries.append(Entry(self.screen, width=20, bg=settings.WHITE,
+            tk.Label(self.screen, text=name + ':', fg=settings.WHITE, bg=settings.BLACK,
+                  font=settings.menu_font).grid(row=row, column=0, sticky=tk.W)
+            self.entries.append(tk.Entry(self.screen, width=20, bg=settings.WHITE,
                                       font=settings.entry_font))
-            self.entries[-1].grid(row=row, column=1, sticky=W)
+            self.entries[-1].grid(row=row, column=1, sticky=tk.W)
 
-    def display_radio_buttons(self):
+    def disp_rad_buttons(self):
         # Determines whether the yearly contribution is added to start or end
         # of the year
-        Label(self.screen, text='Contribution Timing' + ':', fg=settings.WHITE, bg=settings.BLACK, font=settings.menu_font) \
-            .grid(row=5, column=0, sticky=W)
+        tk.Label(self.screen, text='Contribution Timing' + ':',
+                 fg=settings.WHITE, bg=settings.BLACK, font=settings.menu_font)\
+                 .grid(row=5, column=0, sticky=tk.W)
         # Add the buttons into the same cell
-        framing = Frame(self.screen, bg=settings.BLACK)
-        framing.grid(row=5, column=1, sticky=NSEW)
-        self.radvar = IntVar()
-        radio1 = Radiobutton(framing, text='End of year', width=12,
-                             indicatoron=0, variable=self.radvar, value=0)
-        radio2 = Radiobutton(framing, text='Start of year',
-                             width=12, indicatoron=0, variable=self.radvar, value=1)
+        framing = tk.Frame(self.screen, bg=settings.BLACK)
+        framing.grid(row=5, column=1, sticky=tk.NSEW)
+        self.toggle_contr = tk.IntVar()
+        radio1 = tk.Radiobutton(framing, text='End of year', width=12,
+                             indicatoron=0, variable=self.toggle_contr, value=0)
+        radio2 = tk.Radiobutton(framing, text='Start of year', width=12,
+                             indicatoron=0, variable=self.toggle_contr, value=1)
         radio1.pack(side='left')
         radio2.pack(side='right')
         print('Radios constructed')
 
-    def output_ending_capital(self):
+    def out_ending_cap(self):
         # Display the ending capital in textbox
-        Label(self.screen, text='\nEnding Capital:', fg=settings.WHITE,
-              bg=settings.BLACK, font=settings.menu_font).grid(row=7, column=0, sticky=NSEW)
-        self.ending_capital_disp = Text(self.screen, width=20, height=1, wrap=WORD,
-                                        background=settings.WHITE, font=settings.entry_font)
-        self.ending_capital_disp.grid(row=7, column=1, sticky=NSEW)
+        tk.Label(self.screen, text='\nEnding Capital:', fg=settings.WHITE,
+                 bg=settings.BLACK, font=settings.menu_font)\
+                 .grid(row=7, column=0, sticky=tk.W)
+        self.ending_capital_disp = tk.Text(
+            self.screen, width=16, height=2, wrap=tk.WORD,
+            background=settings.WHITE, font=settings.entry_font)
+        self.ending_capital_disp.grid(row=7, column=1, sticky=tk.S)
 
-    def display_sequence_buttons(self):
-        # Next Year, New Sequence
-        Button(self.screen, text='New Sequence', width=14,
-               command=self.calculate_ending_capital).grid(row=6, column=0, sticky=N, pady=10)
-        Button(self.screen, text='Add Sequence', width=14,
-               command=self.calculate_ending_capital).grid(row=6, column=1, sticky=N, pady=10)
+    def disp_submit_buttons(self):
+        # Build the graph button
+        tk.Button(self.screen, text='Build Graph', width=10,
+                  command=self.build_graph, font=settings.entry_font)\
+                  .grid(row=6, column=0, sticky=tk.N, pady=10)
+        # Calculate the ending capital
+        tk.Button(self.screen, text='Submit', width=10,
+                  command=self.calc_ending_cap, font=settings.entry_font)\
+            .grid(row=6, column=1, sticky=tk.N, pady=10)
 
-    def calculate_ending_capital(self):
-        print('Calculate ending capital called')
-        #
-        capital = self.entries[0].get()
-        rate = self.entries[1].get()
-        num_periods = self.entries[2].get()
-        contribution = self.entries[3].get()
-        capital, rate, num_periods, contribution = list(
-            map(int, [capital, rate, num_periods, contribution]))
-        #
+    def get_entries(self):
+        # Grab all the entries from entry boxes
+        self.capital = self.entries[0].get()
+        self.rate = self.entries[1].get()
+        self.num_periods = self.entries[2].get()
+        self.contribution = self.entries[3].get()
+        self.convert_entries_int()
+
+    def convert_entries_int(self):
+        # Convert parameters to int
         try:
-            for year in range(num_periods):
-                if self.radvar.get():
-                    capital = (capital + contribution) * (1 + rate / 100)
-                else:
-                    capital = capital * (1 + rate / 100) + contribution
-                # Record each capital
-            capital = '$' + str("{:,}".format(round(capital)))
+            self.capital, self.rate, self.num_periods, self.contribution = list(
+                map(int, [self.capital, self.rate, self.num_periods, self.contribution]))
         except:
-            capital = 'Please enter numbers only'
-            raise Exception('Non-number entered in entry box')
-        self.display_text(capital, self.ending_capital_disp)
+            self.capital = 'Please enter numbers only'
+
+    def calc_ending_cap(self):
+        self.get_entries()
+        # Display error message
+        if isinstance(self.capital, str):
+            self.display_text(self.capital, self.ending_capital_disp)
+        # Calculate ending capital then display final amount
+        else:
+            self.cap_list = [self.capital]
+            for self.year in range(self.num_periods):
+                if self.toggle_contr.get():
+                    self.capital = (self.capital + self.contribution)\
+                     * (1 + self.rate / 100)
+                else:
+                    self.capital = self.capital * \
+                        (1 + self.rate / 100) + self.contribution
+                self.cap_list.append(self.capital)
+            # Record each capital
+            self.capital = '$' + str("{:,}".format(round(self.capital)))
+            self.display_text(self.capital, self.ending_capital_disp)
+
+    def graph_exception(self):
+        return False
+
+    def total_contri_generator(self):
+        for x in range(self.num_periods+1):
+            yield self.capital + x*self.contribution
+
+    def build_graph(self):
+        self.get_entries()
+        # Total contribution line
+        if self.graph_exception():
+            # Output error to textbox
+            return
+        total_contributions = list(self.total_contri_generator())
+        time_period = range(self.num_periods+1)
+        # Create graph and display
+        new = mla.LineGraph()
+        new.plot_line(time_period, self.cap_list, 'Total Capital')
+        new.plot_line(time_period, total_contributions, 'Total Contribution')
+        new.show_graph()
+
+    def disp_back_button(self):
+        tk.Button(self.screen, text='<-- back', width=10,
+                  command=self.back_to_main, font=settings.entry_font)\
+            .grid(row=8, column=0, sticky=tk.W, pady=10)
+
+    def back_to_main(self):
+        self.screen.destroy()
+        calculator.MainScreen()
 
 
 if __name__ == '__main__':
-    start_calc()
+    YearBy()
